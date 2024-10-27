04405169b92d5707f98f4e3228949a7f3915c06d
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2024-10-27 15:57:20 +0200

    drew pipes and bird with simple shapes and fixed mistakes

--- FlappyBird/FlappyBird/FlappyBird.cpp
+++ FlappyBird/FlappyBird/FlappyBird.cpp
@@ -5,18 +5,20 @@ using namespace std;
 #include <string>
 #include <fstream>
 #include <memory>
-#define WINDOW_HEIGHT 600
-#define WINDOW_WIDTH 800
-#define GRAVITY 0.25
-#define BIRD_HEIGHT 20 
-#define BIRD_WIDTH 20
-#define MIN_SPACE_BETWEEN_PIPES 40
-#define PIPE_MAX_HEIGHT 550
-#define PIPE_MIN_HEIGHT 10
-#define PIPE_MAX_WIDTH 30
-#define PIPE_MIN_WIDTH 10
-#define  SOFT_PIPE_CHANCE  50
-
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 1400
+#define GRAVITY 0.005
+#define BIRD_HEIGHT 40 
+#define BIRD_WIDTH 40
+#define MIN_SPACE_BETWEEN_PIPES 100
+#define PIPE_MAX_HEIGHT 600
+#define PIPE_MIN_HEIGHT 100
+#define PIPE_MAX_WIDTH 70
+#define PIPE_MIN_WIDTH 40
+#define SOFT_PIPE_CHANCE  50
+#define VELOCITY_Y 0
+#define VELOCITY_X -1
+#define JUMP -0.75
 class Entity
 {
 protected:
@@ -48,9 +50,9 @@ public:
 	{
 		return height;
 	}
-	virtual void updatePosition(const int value) = 0;
 	
 };
+
 enum PipeType {
 	Top,
 	Bottom
@@ -69,6 +71,7 @@ class Pipe: public Entity {
 
 		return false;
 	};
+
 	bool soft = makeSoft();
 
 public:
@@ -88,7 +91,7 @@ public:
 	{
 		return type;
 	}
-	void updatePosition(const int value)override
+	void updatePosition(const int value)
 	{
 		x += value;
 	}
@@ -99,16 +102,30 @@ public:
 	}
 };
 
-struct Bird: public Entity
+class Bird: public Entity
 {
+	double velocityY = VELOCITY_Y;
+
+public:
 	Bird(): Entity(WINDOW_WIDTH / 5, WINDOW_HEIGHT / 2, BIRD_WIDTH, BIRD_HEIGHT) {}
+
 	void flapWings()
 	{
-		y-=10;
-	};
-	void updatePosition(const int value)override
+		velocityY = JUMP;
+	}
+
+	void updatePosition()
 	{
-		y += value;
+		y += velocityY;
+	}
+	
+	void updateVelocity(const double value)
+	{
+		velocityY += value;
+	}
+	void setVelocity(const double value)
+	{
+		velocityY = value;
 	}
 };
 
@@ -136,15 +153,18 @@ public:
 		currentScore+= value;
 		return *this;
 	}
+
 	const Score& operator=(const double value)
 	{
 		currentScore = value;
 		return *this;
 	}
+
 	const double getScore() const
 	{
 		return currentScore;
-	};
+	}
+
 	void saveBestScore() const
 	{
 		const double previous_best_score = getBestScore();
@@ -163,17 +183,17 @@ public:
 		cout << "The score was loaded successfully!" << endl;
 		file.close();
 
-	};
+	}
 };
 
-class GameEngine {
+class GameEngine 
+{
 	static GameEngine* instance;
 	sf::Clock clock;
 	vector<Pipe> pipes;
 	Bird bird;
 	Score score;
-	double velocityX = -2;
-	double velocityY = -1;
+	double velocityX = VELOCITY_X;
 	sf::Time lastPipeSGenerated;
 
 	void increaseSpeed()
@@ -184,7 +204,7 @@ class GameEngine {
 		if (elapsedTime.asSeconds() - lastUpdate.asSeconds() >= 50)
 		{
 			velocityX -= 0.25;
-			velocityY += 0.25;
+			bird.updateVelocity(0.25);
 			lastUpdate = elapsedTime;
 		}
 	};
@@ -193,7 +213,7 @@ class GameEngine {
 	void generatePipes()
 	{
 		int topPipeHeight = rand() % (PIPE_MAX_HEIGHT - PIPE_MIN_HEIGHT + 1) + PIPE_MIN_HEIGHT;
-		int topY = topPipeHeight-1;
+		int topY = 0;
 		int bottomPipeHeight = rand() % (PIPE_MAX_HEIGHT - topPipeHeight - PIPE_MIN_HEIGHT + 1) + PIPE_MIN_HEIGHT;
 		int bottomY = WINDOW_HEIGHT - bottomPipeHeight - 1;
 		int width = rand() % (PIPE_MAX_WIDTH - PIPE_MIN_WIDTH + 1)+ PIPE_MIN_WIDTH;
@@ -202,35 +222,48 @@ class GameEngine {
 		Pipe bottomPipe(bottomY, width, bottomPipeHeight, Bottom);
 		pipes.push_back(topPipe);
 		pipes.push_back(bottomPipe);
-	};
-
+	}
 
 	GameEngine()=default;
+
 public:
 
 	const vector<Pipe>& getPipes() const 
 	{
 		return pipes;
-	};
+	}
+
 	bool collisionCanvas()const
 	{
 		return bird.getY() + bird.getHeight() / 2 >= WINDOW_HEIGHT || bird.getY() - bird.getHeight() / 2 <= 0;
 	}
-	bool collisionPipe(const Pipe& pipe) const
-	{
-		const int pipeTopY = (pipe.getType () == Bottom) ? pipe.getY() - pipe.getHeight() : pipe.getY();
-		const int pipeBottomY = (pipe.getType() == Bottom) ? pipe.getY() : pipe.getY() + pipe.getHeight();
 
-		if (bird.getX() - bird.getWidth() / 2 < pipe.getX() + pipe.getWidth() &&
-			bird.getX() + bird.getWidth() / 2 > pipe.getX() &&
-			bird.getY() - bird.getHeight() / 2 < pipeBottomY &&
-			bird.getY() + bird.getHeight() / 2 > pipeTopY)
-		{
-			return true; 
-		}
-		return false;
-		
-	};
+	bool collisionPipe(const Pipe& pipe) const
+{
+    const int pipeX = pipe.getX();
+    const int pipeY = pipe.getY();
+
+    const int pipeRightX = pipe.getX() + pipe.getWidth();
+    const int pipeBottomY = pipeY +  pipe.getHeight(); 
+
+    if (bird.getX() - bird.getWidth() / 2 <= pipeRightX &&
+        bird.getX() + bird.getWidth() / 2 >= pipe.getX())
+    {
+        if (pipe.getType() == Top && bird.getY() - bird.getHeight() / 2 < pipeBottomY && !pipe.checkIfSoft())
+        {
+			cout << "Collision with a pipe, bird x -  " << bird.getX() << ", pipe left x - " << pipeX << ", pipe right x" << pipeRightX << endl;
+			cout << "Collision with a top pipe, bird y -  " << bird.getY() << ", pipe bottom y - "  << pipeBottomY << endl;
+            return true; 
+        }
+        if (pipe.getType() == Bottom && bird.getY() + bird.getHeight() / 2 > pipe.getY() && !pipe.checkIfSoft())
+        {
+			cout << "Collision with a pipe, bird x -  " << bird.getX() << ", pipe left x - " << pipeX << ", pipe right x" << pipeRightX << endl;
+			cout << "Collision with a bottom pipe, bird y -  " << bird.getY() << ", pipe top y - " << pipe.getY()  << endl;
+            return true; 
+        }
+    }
+    return false;
+}
 
 	void go()
 	{
@@ -241,35 +274,30 @@ public:
 			lastPipeSGenerated = currentTime;
 		}
 
-		velocityY += GRAVITY;
-		bird.updatePosition(velocityY);
-		cout << "Bird y:" << bird.getY() << endl;
+		bird.updateVelocity(GRAVITY);
+		bird.updatePosition();
 
 		if (collisionCanvas())
 		{
-			over = true;
+			Over();
 			return;
 		}		
 
 		for (Pipe& pipe : pipes)
 		{
 			pipe.updatePosition(velocityX);
-			cout << "Pipe x:" << bird.getX() << ", width " << pipe.getWidth() << ", height: " << pipe.getHeight()<< endl;
-
 			if (collisionPipe(pipe))
 			{
 				if (!pipe.checkIfSoft())
 				{
-					over = true;
+					Over();
 					return;
 				}
-				cout << "Coliision" << endl;
 			}
 			else if(!pipe.checkIfPassed() && bird.getX() > pipe.getX() + pipe.getWidth())
 			{
 				pipe.makePassed();
 				score+=0.5;
-				cout << score.getScore() << endl;
 			}
 		}
 		
@@ -282,30 +310,41 @@ public:
 		generatePipes();
 		go();
 		
-	};
+	}
+
 	bool over = false;
 	bool started = false;
+
 	void reset()
 	{
 		score.saveBestScore();
 		pipes.clear();
 		score = 0;
 		start();
-	};
+	}
+
 	void flap() 
 	{
 		bird.flapWings();
-		cout << "FlapBird y:" << bird.getY() << endl;
 
 		if (collisionCanvas())
 		{
 			over = true;
-			cout << "coliision" << endl;
-		};
-		
+			cout << "coliision with canvas" << endl;
+		}
 	}
 
-	
+	void Over()
+	{
+		over = true;
+		score.saveBestScore();
+	}
+
+	const Bird& getBird()const
+	{
+		return bird;
+	}
+
 	static GameEngine* getInstance()
 	{
 		if (instance == nullptr)
@@ -314,7 +353,7 @@ public:
 			instance = new GameEngine();
 		}
 		return instance;
-	};
+	}
 };
 GameEngine* GameEngine::instance = nullptr;
 
@@ -324,18 +363,39 @@ private:
 	static Render* instance;
 
 	sf::RenderWindow window;
-	Render() : window(sf::VideoMode(WINDOW_HEIGHT, WINDOW_WIDTH), "Flappy bird", sf::Style::Close) {}
+	Render() : window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Flappy bird", sf::Style::Close) {}
 
-	void drawPipe(const Pipe& pipe){}
+	void drawPipe(const Pipe& pipe)
+	{
+		sf::RectangleShape pipe_shape(sf::Vector2f(pipe.getWidth(), pipe.getHeight()));
+		pipe.checkIfSoft()? pipe_shape.setFillColor(sf::Color::Yellow): pipe_shape.setFillColor(sf::Color::Cyan);
+			pipe_shape.setPosition(pipe.getX(), pipe.getY());
+			window.draw(pipe_shape);
+		
+
+		
+	}
 	void drawBird(const Bird& bird)
-	{}
+	{
+		sf::RectangleShape bird_shape(sf::Vector2f(bird.getWidth(), bird.getHeight()));
+		bird_shape.setFillColor(sf::Color::Magenta);
+		bird_shape.setOrigin(bird.getWidth()/2.f, bird.getHeight() / 2.f);
+		bird_shape.setPosition(bird.getX(), bird.getY());
+		window.draw(bird_shape);
+	}
 	void manageWindow()
 	{}
 	void displayGameOver()
 	{}
 	void render(GameEngine* game)
 	{
-	
+		window.clear();
+		drawBird(game->getBird());
+		for (const Pipe& pipe : game->getPipes())
+		{
+			drawPipe(pipe);
+		}
+		window.display();
 	}
 	void processEvents(GameEngine* game)
 	{		
@@ -344,7 +404,7 @@ private:
 		{
 			if (event.type == sf::Event::Closed)
 			{
-				game->over = true;
+				game->Over();
 				window.close();
 			}
 			else if ((event.type == sf::Event::MouseButtonPressed || event.type == sf::Event::KeyPressed) && !game->started)
@@ -391,11 +451,8 @@ public:
 			processEvents(game);
 			if (!game->over && game->started)
 			{
-				game->go(); // Update game logic in every frame
-				if (game->over)
-				{
-					cout << "over" << endl;
-				}
+				game->go(); 
+				
 			}
 			render(game);
 		}
